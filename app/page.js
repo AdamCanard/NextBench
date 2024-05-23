5f623bbdc373dcf8d913dc6b4f1af11cb7ba674d
AdamCanard <azcunard@gmail.com>
2024-05-23 12:24:52 -0600

    stolen valor

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ServerSideBox from "@/components/ServerSideBox";
+
+export default function Home() {
+  return (
+    <main className="">
+        <ServerSideBox/>
+    </main>
+  );
+}
